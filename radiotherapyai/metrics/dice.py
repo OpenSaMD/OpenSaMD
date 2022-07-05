ed1c58ef03133453d6674bd4677d10ba946c0495
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-07-05 11:02:38 +1000

    Add Dice metric and test

---
+++ radiotherapyai/metrics/dice.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2022 Radiotherapy AI Pty Ltd
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import shapely.geometry
+
+
+def dice_from_polygons(
+    a: shapely.geometry.polygon.LinearRing, b: shapely.geometry.polygon.LinearRing
+) -> float:
+    """Determine the Dice coefficient metric comparing two shapely polygons.
+
+    Explanation of the Dice coefficient is available at:
+    <https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient>
+
+    Args:
+        a (shapely.geometry.polygon.LinearRing)
+        b (shapely.geometry.polygon.LinearRing)
+    """
+
+    return 2 * a.union(b).area / (a.area + b.area)
