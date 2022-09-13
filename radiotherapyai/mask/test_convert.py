d635ae7c8cbbecf1d4088075cd125303eda53d5d
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-09-13 16:16:18 +1000

    add keyhole note

--- radiotherapyai/mask/test_convert.py
+++ radiotherapyai/mask/test_convert.py
@@ -21,9 +21,41 @@ from radiotherapyai.metrics import dice
 from .convert import contours_to_mask, mask_to_contours
 
 
+def test_mask_at_edge_of_image():
+    """Test the effect of a contour at the edge of an image"""
+    mask = np.array(
+        [
+            [255, 255, 127, 0],
+            [255, 255, 127, 0],
+            [127, 127, 64, 0],
+            [0, 0, 0, 0],
+        ],
+        dtype=np.uint8,
+    )
+
+    y_grid = np.array([10, 12, 14, 16])
+    x_grid = np.array([0, 1, 2, 3])
+
+    ideal_expected_contour = np.array([(9, -0.5), (9, 2), (14, 2), (14, -0.5)])
+    ideal_contours = [ideal_expected_contour]
+
+    # Marching squares doesn't perfectly follow the in-out proportion,
+    # instead, it cuts off corners.
+    marching_squares_expected_contour = np.array(
+        [(9, 0), (10, -0.5), (12, -0.5), (14, 0), (14, 1), (12, 2), (10, 2), (9, 1)]
+    )
+    marching_squares_expected_contours = [marching_squares_expected_contour]
+
+    contours = mask_to_contours(x_grid, y_grid, mask)
+
+    assert dice.from_contours(a=contours, b=ideal_contours) > 0.89
+    assert dice.from_contours(a=contours, b=marching_squares_expected_contours) > 0.99
+
+
 def test_conversion_round_trip():
     """Test a round trip of contours -> mask -> contours"""
 
+    # An offset ellipse
     t = np.linspace(0, 2 * np.pi, endpoint=False)
     x = 1.5 * np.sin(t)
     y = np.cos(t) + 0.5
@@ -41,3 +73,11 @@ def test_conversion_round_trip():
     round_trip_contours = mask_to_contours(x_grid, y_grid, mask)
 
     assert dice.from_contours(a=contours, b=round_trip_contours) > 0.99
+
+
+# For this test to pass, need to implement contour keyhole technique.
+# https://dicom.nema.org/medical/Dicom/2022b/output/chtml/part03/sect_C.8.8.6.3.html
+# I'll include that in a follow up PR, given this PR is getting large
+# already.
+def test_round_trip_with_nested_contours():
+    """Test round trip with nested contours"""
