d4b9564261f5e158ba012849b64df2616631cbc9
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-09-20 13:20:12 +1000

    add docstring to _paths module

--- radiotherapyai/_cli/_paths.py
+++ radiotherapyai/_cli/_paths.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Repository paths utilised by the CLI module"""
+
 import pathlib
 
 HERE = pathlib.Path(__file__)
