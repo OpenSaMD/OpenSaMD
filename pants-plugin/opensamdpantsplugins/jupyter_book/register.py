d53133ce42126ef1bfa323dec150d649cd73f1f4
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-11 08:57:53 +1100

    fixing pylint

--- pants-plugin/opensamdpantsplugins/jupyter_book/register.py
+++ pants-plugin/opensamdpantsplugins/jupyter_book/register.py
@@ -1,8 +1,26 @@
-# This registers all of the plugin hooks that action our sources/dependencies
+# Copyright 2022 OpenSaMD contributors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Pants plugin registration"""
+
+
 def rules():
+    """Rule registration"""
     return []
 
 
-# This registers our new plugin's target(s) (used in BUILD files)
 def target_types():
+    """Target registration"""
     return []
