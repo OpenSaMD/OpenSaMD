2b68aa814593676bed288e8c1063a7e2f228806b
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-11 08:48:03 +1100

    set up dummy plugin for pypi publishing

---
+++ pants-plugin/opensamdpantsplugins/__init__.py
@@ -0,0 +1,3 @@
+"""Pants plugins created within the OpenSaMD project"""
+
+__version__ = "0.1.0-dev0"
---
+++ pants-plugin/opensamdpantsplugins/jupyter_book/register.py
@@ -0,0 +1,8 @@
+# This registers all of the plugin hooks that action our sources/dependencies
+def rules():
+    return []
+
+
+# This registers our new plugin's target(s) (used in BUILD files)
+def target_types():
+    return []
